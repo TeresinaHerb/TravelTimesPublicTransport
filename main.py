bd1285069cc4e7b82c1e63918b42b97453a654fb
TeresinaHerb <teresina.herb@gmail.com>
2019-08-06 19:45:19 +0000

    continue with getStopListNRW functionality

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
-from travel_func import getStopListCity, calcTravelTimes
+from travel_func import getStopListCity, calcTravelTimes, getStopListNRW
+
+path = '/workspace/TravelTimesPublicTransport/mainStops.csv'
+stopListNRW = getStopListNRW(path)
+#print(stopListNRW)
 
 # get all the stops in City 5111 (Bochum)
-stopList = getStopListCity(5911)
+stopList1 = getStopListCity(5911)
 
 # set start stop to which travel times shall be calculated: Bochum Hbf
 # get value out of dictionary
 start = "Hbf"
-for stopID, name in stopList.items():
+for stopID, name in stopList1.items():
     if name == start:
         startStopID = stopID
         print(startStopID)
 
-# calculate all travel times
-travel_t = calcTravelTimes(startStopID)
+# calculate travel times for big cities
+dateV = '20190909'
+timeV = '06:00'
+#print(len(stopList1))
+#travelT = calcTravelTimes(startStopID, dateV, timeV)
 
 # next steps:
 # generate a complete list of all stops in NRW
--- travel_func.py
+++ travel_func.py
@@ -2,18 +2,39 @@ import csv
 import requests
 from xml.etree import ElementTree
 
-def calcTravelTimes(stopID):
+stopList = {}
+
+def calcTravelTimes(stopID, dateV, timeV):
     travel_times = {}
+    for startID, name in stopList.items():
+        url = 'https://openservice-test.vrr.de/static02/XML_TRIP_REQUEST2?requestID=12827&locationServerActive=1'
+        url += '&name_origin=' + startID + '&type_origin=stopID'
+        url += '&name_destination=' + stopID + '&type_destination=stopID'
+        url += '&stateless=1&itdTripDateTimeDepArr=dep&calcOneDirection=1&useRealtime=1'
+        url += '&itdDate=' + dateV
+        url += '&itdTime=' + timeV
+        url += '&outputFormat=XML&language=de'
+
+        # send out request
+        r = requests.get(url)
+        efa = ElementTree.fromstring(r.content)
+        print(efa)
+        # print the travelTimes
+        #for child in efa.iter('publicDuration'):
+            #print(child)
+            #print(child.attrib.get('stopID'), child.text)
+            #stopList[child.attrib.get('stopID')] = child.text
     return travel_times
 
 def getStopListCity(cityID):
-    stopList = {}
+    global stopList
+    stopList= {}
     # send request to EFA Backend with cityID
     url = 'https://openservice-test.vrr.de/static02/XML_STOPLIST_REQUEST?'
-    url += 'stopListOMC={:d}'.format(cityID)
+    url += 'stopListOMC=' + str(cityID)
     url += '000'
+    #print(url)
 
-    print(url)
     # send out request
     r = requests.get(url)
     efa = ElementTree.fromstring(r.content)
@@ -24,7 +45,7 @@ def getStopListCity(cityID):
         stopList[child.attrib.get('stopID')] = child.text
     return stopList
 
-def getStopListNRW(path='/TravelTimesPublicTransport/mainStops.csv'):
+def getStopListNRW(path):
     stopListNRW = {}
     # read csv and request line by line
     with open(path, 'r') as f:
@@ -32,8 +53,14 @@ def getStopListNRW(path='/TravelTimesPublicTransport/mainStops.csv'):
         # set up new dictionaries for results
         # key: efaID, value: stop name
         for row in reader:
-            cityStops = {}
-            cityID = row[0]
-            cityStops = getStopListCity(cityID)
-            stopListNRW.update(cityStops)
+            temp = row[0].split(";")
+            cityID = temp[0]
+            #city = temp[1]
+            #stopListNRW[cityID] = city
+            stopList_temp = getStopListCity(cityID)
+            if len(stopList_temp) == 0:
+                print(temp[1])
+            else:
+                stopListNRW.update(stopList_temp)
+                print(len(stopListNRW))
     return stopListNRW
